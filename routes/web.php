635f9a500d91e7f5be294a89ac50cb486aa66cd7
cham <ankit.rathore@engineermaster.in>
2021-11-18 13:51:25 +0530

    code

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function() {
+    redirect('/login');
+});
+
 Auth::routes();
 Route::get('/admin/{any}', [App\Http\Controllers\HomeController::class, 'index'])->where('any', '.*')->name('home');
